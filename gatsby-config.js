24220d3fbc54a56edc75fdf6667eda5f822626f3
Thomas Friedrichs <friedrichs.tommy@gmail.com>
2021-10-11 13:04:05 -0700

    update: add navbar

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Travel Site`,
+    description: `Travvel blog`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
---
+++ src/components/header/HeaderElement.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import { Link } from 'gatsby';
+
+export const Nav = styled.nav`
+  background: red;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5rem calc((100vw - 1300px) / 2);
+  z-index: 100;
+  position: relative;
+`
+
+export const NavLink = styled(Link)`
+  color: white;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+`
